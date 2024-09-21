c63213d7669db764b7253210df391df53d42d771
georgepoyilil <georgepoyilil@gmail.com>
2024-09-21 08:40:46 +0530

    footer added

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -14,6 +14,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { RouterModule } from '@angular/router';
 import { AgeGroupsComponent } from './age-groups/age-groups.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
@@ -31,7 +32,8 @@ import { HomeComponent } from './home/home.component';
     CommonModule,
     RouterModule,
     AgeGroupsComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   // animations:[
   //   trigger('fadeInOut', [
